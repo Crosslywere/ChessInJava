a99870367dfca379fc50667cbe70c400acfd64be
Crosslywere <ogboruj@outlook.com>
2025-04-08 18:19:27 +0100

    Implemented a piece tracker in ChessBoard

--- src/main/java/com/crossly/chess/ChessBoard.java
+++ src/main/java/com/crossly/chess/ChessBoard.java
@@ -6,8 +6,12 @@ import org.joml.Matrix4f;
 import org.joml.Vector3f;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChessBoard {
+
+	private final Map<Integer, ChessPiece> boardMap = new HashMap<>(64);
 
 	private final static Shader BOARD_SHADER;
 
@@ -26,8 +30,11 @@ public class ChessBoard {
 				"""
 						#version 330 core
 						layout (location = 0) out vec4 oFragColor;
+						layout (location = 1) out int oID;
+						uniform int uID;
 						uniform vec3 uColor;
 						void main() {
+							oID = uID;
 							oFragColor = vec4(uColor, 1.0);
 						}
 						""",
@@ -36,7 +43,22 @@ public class ChessBoard {
 	}
 
 	public ChessBoard(ArrayList<ChessPiece> chessPieces) {
-		// TODO Record chess board pieces into a map for position IDs for future logic
+		for (int y = 0; y < 8; y++) {
+			for (int x = 0; x < 8; x++) {
+				int id = (x << 4) + y;
+				ChessPiece posPiece = null;
+				for (var cp : chessPieces) {
+					var position = cp.getPosition();
+					int posX = (int) position.x();
+					int posY = (int) position.y();
+					if (posX == x && posY == y) {
+						posPiece = cp;
+						break;
+					}
+				}
+				boardMap.put(id, posPiece);
+			}
+		}
 	}
 
 	public void drawBoard(Matrix4f projView, Vector3f offset) {
@@ -51,6 +73,11 @@ public class ChessBoard {
 				Matrix4f model = new Matrix4f().translate(x, 0f, z).translate(offset);
 				BOARD_SHADER.setMatrix4("uModel", model);
 				BOARD_SHADER.setFloat3("uColor", color);
+				var cp = boardMap.get((x << 4) + z);
+				if (cp != null)
+					BOARD_SHADER.setInt("uID", cp.getId());
+				else
+					BOARD_SHADER.setInt("uID", -1);
 				Mesh.UNIT_2D_MESH.draw();
 				t = !t;
 				color = t ? new Vector3f(.8f, .75f, .7f) : new Vector3f(.22f, .18f, .1f);
