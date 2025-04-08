4dc6c1e9ba5ac2c3956bb310e78e6ba948676a42
Crosslywere <ogboruj@outlook.com>
2025-04-08 18:23:14 +0100

    Added more features towards completion - Initialized all chess board pieces - Implemented view switching

--- src/main/java/com/crossly/chess/Chess.java
+++ src/main/java/com/crossly/chess/Chess.java
@@ -23,9 +23,7 @@ public class Chess extends Engine {
 	private Vector2f startTextPos = new Vector2f(8f, 96f);
 	private final static float HEADER = 128f;
 	private final static float NORMAL = 64f;
-
 	private ChessBoard chessBoard;
-	private Vector3f selectedColor = new Vector3f();
 
 	public Chess() {
 		super();
@@ -39,10 +37,25 @@ public class Chess extends Engine {
 		framebuffer = new IdFramebuffer(getWindowWidth(), getWindowHeight());
 		basicShader = new Shader("shader/basic.vert", "shader/basic.frag");
 		for (int i = 0; i < 16; i++) {
-			if (i < 8)
-				chessPieces.add(new ChessPiece(i, ChessPiece.PAWN, new Vector2f(i, 1f), new Vector3f(1, 0, 0)));
-			else
-				chessPieces.add(new ChessPiece(i, ChessPiece.PAWN, new Vector2f(i - 8, 6), new Vector3f(0, .5f, 1)));
+			if (i < 8) {
+				chessPieces.add(new ChessPiece(i, ChessPiece.Type.PAWN, new Vector2f(i, 1), ChessPiece.Side.WHITE));
+				chessPieces.add(new ChessPiece(i + 16, ChessPiece.Type.PAWN, new Vector2f(i, 6), ChessPiece.Side.BLACK));
+			} else if (i == 8 || i == 15) {
+				chessPieces.add(new ChessPiece(i, ChessPiece.Type.ROOK, new Vector2f(i - 8, 0), ChessPiece.Side.WHITE));
+				chessPieces.add(new ChessPiece(i + 16, ChessPiece.Type.ROOK, new Vector2f(i - 8, 7), ChessPiece.Side.BLACK));
+			} else if (i == 9 || i == 14) {
+				chessPieces.add(new ChessPiece(i, ChessPiece.Type.KNIGHT, new Vector2f(i - 8, 0), ChessPiece.Side.WHITE));
+				chessPieces.add(new ChessPiece(i + 16, ChessPiece.Type.KNIGHT, new Vector2f(i - 8, 7), ChessPiece.Side.BLACK));
+			} else if (i == 10 || i == 13) {
+				chessPieces.add(new ChessPiece(i, ChessPiece.Type.BISHOP, new Vector2f(i - 8, 0), ChessPiece.Side.WHITE));
+				chessPieces.add(new ChessPiece(i + 16, ChessPiece.Type.BISHOP, new Vector2f(i - 8, 7), ChessPiece.Side.BLACK));
+			} else if (i == 12) {
+				chessPieces.add(new ChessPiece(i, ChessPiece.Type.QUEEN, new Vector2f(i - 8, 0), ChessPiece.Side.WHITE));
+				chessPieces.add(new ChessPiece(i + 16, ChessPiece.Type.QUEEN, new Vector2f(i - 8, 7), ChessPiece.Side.BLACK));
+			} else {
+				chessPieces.add(new ChessPiece(i, ChessPiece.Type.KING, new Vector2f(i - 8, 0), ChessPiece.Side.WHITE));
+				chessPieces.add(new ChessPiece(i + 16, ChessPiece.Type.KING, new Vector2f(i - 8, 7), ChessPiece.Side.BLACK));
+			}
 		}
 		camera.setPitch(45f);
 		camera.setFovY(35f);
@@ -51,6 +64,7 @@ public class Chess extends Engine {
 		spaceMono = new TextWriter(new FontAtlas("fonts/SpaceMono.ttf", 128f), getWindowWidth(), getWindowHeight());
 		spaceMono.setLineHeight(.75f);
 		chessBoard = new ChessBoard(chessPieces);
+		Framebuffer.setClearColor(.8f, .4f, .1f);
 	}
 
 	@Override
@@ -60,18 +74,17 @@ public class Chess extends Engine {
 		if (input.isButtonJustPressed(Input.MOUSE_BUTTON_LEFT)) {
 			int prevSelectedIndex = selectedIndex;
 			selectedIndex = framebuffer.getId(mousePos.x(), getWindowHeight() - mousePos.y());
-			Vector3f prevColor = selectedColor;
-			// Color selected piece
-			if (selectedIndex > -1 && selectedIndex < chessPieces.size()) {
-				selectedColor = chessPieces.get(selectedIndex).getColor();
-				chessPieces.get(selectedIndex).setColor(new Vector3f(1f));
+			// Selecting a piece
+			if (selectedIndex > -1) {
+				chessPieces.stream().filter(chessPiece -> chessPiece.getId() == selectedIndex).findFirst().ifPresent(ChessPiece::highLight);
 				overlay = false;
 			}
-			// Revert to original piece color
-			if (prevSelectedIndex != selectedIndex && prevSelectedIndex > -1 && prevSelectedIndex < chessPieces.size()) {
-				chessPieces.get(prevSelectedIndex).setColor(prevColor);
+			// Deselecting a piece
+			if (prevSelectedIndex != selectedIndex && prevSelectedIndex > -1) {
+				chessPieces.stream().filter(chessPiece -> chessPiece.getId() == prevSelectedIndex).findFirst().ifPresent(ChessPiece::deselect);
 			}
 		}
+		// Toggling the overlay
 		if (input.isKeyJustPressed(Input.KEY_SPACE)) {
 			overlay = !overlay;
 			if (overlay) {
@@ -79,11 +92,13 @@ public class Chess extends Engine {
 				overlayTimer = 0f;
 			}
 		}
-		if (overlay && overlayTimer >= 10f) {
+		// Scrolling the overlay after 10s
+		if (overlay && overlayTimer >= 10f)
 			startTextPos.y -= Timer.getDeltaTime() * 100;
-		} else if (overlay) {
+		else if (overlay)
 			overlayTimer += Timer.getDeltaTime();
-		}
+		if (input.isKeyJustPressed(Input.KEY_S))
+			switchSides();
 	}
 
 	@Override
@@ -94,7 +109,7 @@ public class Chess extends Engine {
 			chessBoard.drawBoard(camera.getProjectionViewMatrix(), new Vector3f());
 			basicShader.use();
 			basicShader.setMatrix4("u_ProjView", camera.getProjectionViewMatrix());
-			basicShader.setFloat3("u_LightDir", new Vector3f(1f).normalize());
+			basicShader.setFloat3("u_LightDir", new Vector3f(1, 1, -1).normalize());
 			chessPieces.forEach(cp -> cp.draw(basicShader));
 			if (overlay) drawOverlay();
 		}
@@ -117,6 +132,11 @@ public class Chess extends Engine {
 	@Override
 	public void onExit() {
 		ChessPiece.PAWN.delete();
+		ChessPiece.ROOK.delete();
+		ChessPiece.KNIGHT.delete();
+		ChessPiece.BISHOP.delete();
+		ChessPiece.QUEEN.delete();
+		ChessPiece.KING.delete();
 		playFairDisplay.getFontAtlas().delete();
 		spaceMono.getFontAtlas().delete();
 		framebuffer.delete();
@@ -127,17 +147,31 @@ public class Chess extends Engine {
 		new Chess().play();
 	}
 
+	private ChessPiece.Side currentSide = ChessPiece.Side.WHITE;
+
+	private void switchSides() {
+		if (currentSide == ChessPiece.Side.WHITE) {
+			camera.setPosition(new Vector3f(3.5f, 8f, 12f));
+			currentSide = ChessPiece.Side.BLACK;
+			camera.setYaw(180);
+		} else if (currentSide == ChessPiece.Side.BLACK) {
+			camera.setPosition(new Vector3f(3.5f, 8f, -5f));
+			currentSide = ChessPiece.Side.WHITE;
+			camera.setYaw(0);
+		}
+	}
+
 	private void drawOverlay() {
 		playFairDisplay.setLineHeight(.25f);
-		var endPos = playFairDisplay.writeText("A 3D Chess Game\n", new Vector2f(startTextPos), HEADER, new Vector3f(0f, .5f, 1f));
+		var endPos = playFairDisplay.writeText("A 3D Chess Game\n", new Vector2f(startTextPos), HEADER, new Vector3f(0, .5f, 1));
 		playFairDisplay.setLineHeight(.75f);
-		endPos = playFairDisplay.writeText("By Crosslywere!\n", new Vector2f(8f, endPos.y()), NORMAL, new Vector3f(1f, 1f, 0f));
-		endPos = playFairDisplay.writeText("Click on a piece to select it!\nPress ", new Vector2f(8f, endPos.y()), 64f);
+		endPos = playFairDisplay.writeText("By Crosslywere(Jude Ogboru)!\n", new Vector2f(8, endPos.y()), NORMAL, new Vector3f(1, 1, 0));
+		endPos = playFairDisplay.writeText("Click on a piece to select it!\nClick anywhere to deselect the piece!\nPress ", new Vector2f(8, endPos.y()), 64);
 		endPos = spaceMono.writeText("[Esc]", endPos, NORMAL);
 		endPos = playFairDisplay.writeText(" to exit!\n", endPos, NORMAL);
-		endPos = playFairDisplay.writeText("Press ", new Vector2f(8f, endPos.y()), NORMAL);
+		endPos = playFairDisplay.writeText("Press ", new Vector2f(8, endPos.y()), NORMAL);
 		endPos = spaceMono.writeText("[Space Bar]", endPos, NORMAL);
 		endPos = playFairDisplay.writeText(" to toggle this overlay!", endPos, NORMAL);
-		if (endPos.y() < 0) startTextPos = new Vector2f(8f, getWindowHeight() + HEADER);
+		if (endPos.y() < 0) startTextPos = new Vector2f(8, getWindowHeight() + HEADER);
 	}
 }
--- src/main/java/com/crossly/chess/ChessPiece.java
+++ src/main/java/com/crossly/chess/ChessPiece.java
@@ -9,26 +9,36 @@ import org.joml.Vector3f;
 public class ChessPiece {
 
 	private final int id;
-	private Model model;
+	private Type type;
 	private Vector2f position;
 	private Vector3f color;
 
+	private boolean selected = false;
+	private boolean moved = false;
+	private final Side side;
+
 	public static final Model PAWN;
-//	public static final Model ROOK;
-//	public static final Model KNIGHT;
-//	public static final Model BISHOP;
-//	public static final Model QUEEN;
-//	public static final Model KING;
+	public static final Model ROOK;
+	public static final Model KNIGHT;
+	public static final Model BISHOP;
+	public static final Model QUEEN;
+	public static final Model KING;
 
 	static {
-		PAWN = new Model("ChessPiece/ChessPiece.obj");
+		PAWN = new Model("meshes/Pawn.obj");
+		ROOK = new Model("meshes/Rook.obj");
+		KNIGHT = new Model("meshes/Knight.obj");
+		BISHOP = new Model("meshes/Bishop.obj");
+		QUEEN = new Model("meshes/Queen.obj");
+		KING = new Model("meshes/King.obj");
 	}
 
-	public ChessPiece(int id, Model model, Vector2f position, Vector3f color) {
+	public ChessPiece(int id, Type type, Vector2f position, Side color) {
 		this.id = id;
-		this.model = model;
+		this.type = type;
 		this.position = position;
-		this.color = color;
+		this.color = color.value;
+		this.side = color;
 	}
 
 	public int getId() {
@@ -37,26 +47,73 @@ public class ChessPiece {
 
 	public void draw(Shader shader) {
 		shader.use();
-		shader.setMatrix4("u_Model", new Matrix4f().translate(position.x, 0f, position.y).scale(0.8f));
+		shader.setMatrix4("u_Model", new Matrix4f().translate(position.x, 0f, position.y).scale(0.8f).rotateY((float)Math.toRadians(side == Side.WHITE && type == Type.KNIGHT ? -90 : 0)));
 		shader.setInt("u_Id", id);
 		shader.setFloat3("u_Color", color);
-		model.draw(shader);
+		type.model.draw(shader);
+	}
+
+	public Vector2f getPosition() {
+		return position;
 	}
 
 	public void setPosition(Vector2f position) {
 		this.position = position;
+		moved = true;
 	}
 
-	public Vector3f getColor() {
-		return new Vector3f(color);
+	public void highLight() {
+		selected = true;
+		this.color = new Vector3f(.8f, .6f, 0);
 	}
 
-	public void setColor(Vector3f color) {
-		this.color = color;
+	public void deselect() {
+		selected = false;
+		this.color = side.value;
 	}
 
-	public void setModel(String modelFilepath) {
-		model.delete();
-		model = new Model(modelFilepath);
+	public Side getSide() {
+		return side;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
+	public boolean isSelected() {
+		return selected;
+	}
+
+	public boolean isMoved() {
+		return moved;
+	}
+
+	public enum Side {
+		WHITE(new Vector3f(.8f, .8f, .6f)),
+		BLACK(new Vector3f(.4f));
+
+		private final Vector3f value;
+
+		Side(Vector3f value) {
+			this.value = value;
+		}
+	}
+
+	public enum Type {
+		PAWN(ChessPiece.PAWN),
+		ROOK(ChessPiece.ROOK),
+		KNIGHT(ChessPiece.KNIGHT),
+		BISHOP(ChessPiece.BISHOP),
+		QUEEN(ChessPiece.QUEEN),
+		KING(ChessPiece.KING);
+
+		private final Model model;
+
+		Type(Model model) {
+			this.model = model;
+		}
+		public Model getModel() {
+			return model;
+		}
 	}
 }
